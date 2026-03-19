177f88d582e78f5ce7ef5565fcb1b5704351d640
sheip9 <sheip9@outlook.com>
2026-03-19 11:53:38 +0800

    移除t字段,因为将会在枚举的序列化中进行

--- qqbot_sdk_core/src/events/payload.rs
+++ qqbot_sdk_core/src/events/payload.rs
@@ -3,11 +3,10 @@ use crate::events::opcode::Opcode;
 use crate::EventType;
 
 #[derive(Debug, Serialize, Deserialize)]
-pub struct WebhookPayload<T> {
+pub struct WebhookPayload {
     pub id: Option<String>,
     /// OpCode
     pub op: Opcode,
-    pub d: T,
     pub s: Option<u64>,
     #[serde(flatten)]
     pub event: EventType,
